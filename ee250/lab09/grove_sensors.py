22bbfa675bc24ad9f55bf9754c2ac08641cf2d50
Ashley Ly <lyashley@usc.edu>
2018-04-18 16:54:52 -0700

    grove_sensors.py

--- ee250/lab09/grove_sensors.py
+++ ee250/lab09/grove_sensors.py
@@ -8,7 +8,7 @@ sys.path.append('../..Software/Python/')
 
 # Connect the Grove Temperature Sensor to analog port A0
 # SIG,NC,VCC,GND
-sensor = 3
+dht_sensor = 3
 
 # Connect the Grove LED to digital port D4
 led = 4
@@ -54,7 +54,7 @@ if __name__== '__main__':
 	    
 	    	time.sleep(1)
 
-	    	[ temp,hum ] = dht(sensor,1)
+	    	[ temp,hum ] = dht(dht_sensor,1)
 
 	    	t = str(temp)
 	    	h = str(hum)
