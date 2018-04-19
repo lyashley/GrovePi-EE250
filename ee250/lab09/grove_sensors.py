e6daf5a112b03e8de4dd979ef6121a2c1c126918
Ashley Ly <lyashley@usc.edu>
2018-04-18 17:05:56 -0700

    grove_sensors.py

--- ee250/lab09/grove_sensors.py
+++ ee250/lab09/grove_sensors.py
@@ -13,19 +13,6 @@ dht_sensor = 3
 # Connect the Grove LED to digital port D4
 led = 4
 
-
-
-def on_connect(client, userdata, flagas, rc):
-	print("Connected to server(i.e. broker) with result code" + str(rc))
-	client.subscribe("anrg-pi8/led")
-	client.message_callback_add("anrg-pi8/led", led_callback)
-
-	client.subscribe("anrg-pi8/lcd")
-	client.message_callback_add("anrg-pi8/lcd", lcd_callback)
-
-def on_message(client, userdata, msg):
-	print("on_message:" + msg.topic + " " + str(msg.payload, "utf-8"))
-
 def lcd_callback(client, lcd_data, lcd_msg):
 	lcd_data = str(lcd_msg.payload, "utf-8")
 
@@ -41,7 +28,16 @@ def led_callback(client, led_data, led_msg):
 			else:
 				digitalWrite(led,1)
 
+def on_connect(client, userdata, flagas, rc):
+	print("Connected to server(i.e. broker) with result code" + str(rc))
+	client.subscribe("anrg-pi8/led")
+	client.subscribe("anrg-pi8/lcd")
+	
+	client.message_callback_add("anrg-pi8/led", led_callback
+	client.message_callback_add("anrg-pi8/lcd", lcd_callback)
 
+def on_message(client, userdata, msg):
+	print("on_message:" + msg.topic + " " + str(msg.payload, "utf-8"))
 
 if __name__== '__main__':
 	client = mqtt.Client()
